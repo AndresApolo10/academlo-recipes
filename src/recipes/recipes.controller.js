41b570219121f880e4772af6cf6b668bcaf9ba42
AndresApolo10 <andres-10a@hotmail.com>
2022-11-02 22:58:03 -0500

    add new controller

--- src/recipes/recipes.controller.js
+++ src/recipes/recipes.controller.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid')
+const {Op} = require('sequelize')
 const Categories = require('../models/categories.models')
 const Ingredients = require('../models/ingredients.models')
 const Instructions = require('../models/instructions.models')
@@ -6,6 +7,7 @@ const Recipes = require('../models/recipes.models')
 const RecipesIngredients = require('../models/recipes_ingredients.models')
 const Types = require('../models/types.models')
 const Users = require('../models/users.models')
+const UsersIngredients = require('../models/users_ingredients.models')
 
 const getAllRecipes = async () => {
     const data = await Recipes.findAll({
@@ -42,7 +44,32 @@ const getRecipeById = async (id) => {
     const data = await Recipes.findOne({
         where: {
             id
-        }
+        },
+        attributes: {
+            exclude: ['userId', 'categoryId']
+        },
+        include: [
+            {
+                model: Categories
+            },
+            {
+                model: Users,
+                attributes: ['id', 'firstName', 'lastName']
+            },
+            {
+                model: Instructions,
+                attributes: ['description', 'step']
+            },
+            {
+                model: RecipesIngredients,
+                include: {
+                    model: Ingredients,
+                    include: {
+                        model: Types
+                    }
+                }
+            }
+        ]
     })
     return data
 }
@@ -77,6 +104,34 @@ const deleteRecipe = async (id) => {
     const data = await Recipes.destroy({
         where: {
             id
+        }
+    })
+    return data
+}
+
+const getMyRecipes = async (userId) => {
+    const userIngredients = await UsersIngredients.findAll({
+        attributes: ['ingredientId'],
+        where: {
+            userId
+        }
+    })
+    const filteredIngredients = userIngredients.map(obj => obj.ingredientId)
+    const recipeIngredients = await RecipesIngredients.findAll({
+        where: {
+            ingredientId: {
+                [Op.in]: filteredIngredients
+            }
+        }
+    })
+
+    const filteredRecipes = recipeIngredients.map(obj => obj.recipeId)
+
+    const data = await Recipes.findAll({
+        where: {
+            id: {
+                [Op.in]: filteredRecipes
+            }
         }
     })
     return data
